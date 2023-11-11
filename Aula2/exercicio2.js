288a575925e8a782e30e193b44302afb957fe382
Dayana Mariano <dayanaraquelle@gmail.com>
2023-11-11 20:50:38 -0300

    resposta ex2

--- Aula2/exercicio2.js
+++ Aula2/exercicio2.js
@@ -2,18 +2,15 @@
 // Dado um vetor `notas` com 4 elementos, calcule a média aritmética de todos os valores e armazene o resultado na variável `media`. Utilize o índice para acessar os valores do vetor e realizar os cálculos.
 
 let notas = [17, 28, 39, 96, 55];
-
 let soma = 0;
-let media = 0;
 
-for (let index = 0; index = notas.length; index++) {
+for (let index = 0; index < notas.length; index++) {
   soma += notas[index];
-}
 
-console.log(soma)
+}
 
 let quantidadeNotas = notas.length
 
-// media = soma/quantidadeNotas
+let media = soma/quantidadeNotas
 
-// console.log(media)
+console.log(media)
