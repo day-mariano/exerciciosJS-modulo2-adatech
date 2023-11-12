b7263b110b8afcdd119f591487e1050324b80b95
Dayana Mariano <dayanaraquelle@gmail.com>
2023-11-11 23:32:26 -0300

    resposta ex4

---
+++ Aula2/exercicio4.js
@@ -0,0 +1,19 @@
+// ### Exercício 4: Produto dos elementos de uma matriz
+// Dada uma matriz `matriz` com 3 linhas e 3 colunas, calcule o produto de todos os valores e armazene o resultado na variável `produto`. Utilize o índice para acessar os valores da matriz e realizar os cálculos.
+
+const matriz = [
+  [2, 4, 6],
+  [3, 7, 9],
+  [5, 8, 10]
+];
+
+let produto = 1
+
+for (let i = 0; i < matriz.length; i++) {
+
+  for (let j = 0; j < matriz[i].length; j++) {
+    produto = produto * matriz[i][j]
+  }
+}
+
+console.log(produto)
