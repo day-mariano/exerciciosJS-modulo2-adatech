d05cae144eaca0594ad9cf23e505c24376da3b65
Dayana Mariano <dayanaraquelle@gmail.com>
2023-11-12 19:27:15 -0300

    resposta ex 1

---
+++ Aula4/aula4ex1.js
@@ -0,0 +1,12 @@
+// 1. **Exercício Fácil:**
+// Escreva um programa em JavaScript que verifica se um número é positivo, negativo ou igual a zero. Use a estrutura `if-else` para realizar essa verificação.
+
+let numero = 4
+
+if (numero > 0) {
+  console.log('O número é positivo')
+} else if (numero < 0) {
+  console.log("O número é negativo")
+} else {
+  console.log("O número é igual a 0")
+}
